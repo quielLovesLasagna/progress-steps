b0a9eb38c847e8a54ddda141377558e331617e51
quielLovesLasagna <exequielarco23@gmail.com>
2023-12-08 11:25:03 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Progress Steps
+
+A card simple card component that expands smoothly.
+
+Live site URL: []()
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Features](#features)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### Features
+
+Users should be able to:
+
+- Click the previous and next button
+- See the progress line and circles change color
+
+## My process
+
+### Built with
+
+- HTML5
+- CSS3
+- JavaScript
+
+## Author
+
+- GitHub - [quielLovesLasagna](https://github.com/quielLovesLasagna)
+
+## Acknowledgements
+
+I would like to thanks **Brad Traversy** and **Florin Pop** for the guidance throughout the project.
---
+++ script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// Element/s
+const progress = document.getElementById("progress");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+// State/s
+let currentActive = 1;
+
+// Function/s
+function update() {
+	circles.forEach((circle, index) => {
+		if (index < currentActive) {
+			circle.classList.add("active");
+		} else {
+			circle.classList.remove("active");
+		}
+	});
+
+	const actives = document.querySelectorAll(".active");
+
+	progress.style.width =
+		((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+	if (currentActive === 1) {
+		prevBtn.disabled = true;
+	} else if (currentActive === circles.length) {
+		nextBtn.disabled = true;
+	} else {
+		prevBtn.disabled = nextBtn.disabled = false;
+	}
+}
+
+// Event listener/s
+nextBtn.addEventListener("click", function () {
+	currentActive++;
+
+	if (currentActive > circles.length) {
+		currentActive = circles.length;
+	}
+
+	update();
+});
+
+prevBtn.addEventListener("click", function () {
+	currentActive--;
+
+	if (currentActive < 1) {
+		currentActive = 1;
+	}
+
+	update();
+});
